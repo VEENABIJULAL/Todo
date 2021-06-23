aeb87315003db56c3832ca75b25de2e05ff8b457
veenabijulal <veenas7854@gmail.com>
2021-06-23 21:03:58 +0530

    two way binding and login function created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { TodoComponent } from './todo/todo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,12 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+uid="User-id please";
+pswd="";
+
+accountdetails:any={
+  1000: {uid:1000, username:"userone", password:"userone",todo:""},
+  1001: {uid:1001, username:"usertwo", password:"usertwo", todo:""},
+  1002: {uid:1002, username:"userthree", password:"userthree", todo:""},
+  1003: {uid:1003, username:"userfour", password:"userfour", todo:""},
+}
+
   constructor() { }
 
   ngOnInit(): void {
   }
   login(){
-    alert("register clicked");
+    var uid=this.uid;
+    var pswd=this.pswd;
+    let users=this.accountdetails;
+    if(uid in users){
+      if(pswd==users[uid]["password"]){
+        alert("login successful");
+      }else{
+        alert("Incorrect password");
+      }
+    }
+    else{
+      alert("Invalid account");
+    }
+    
   }
   register(){
     alert("register clicked");
